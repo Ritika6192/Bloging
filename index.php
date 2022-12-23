c097bec1f0d0b8d99fcd9948e460a17a80f8ec94
Ritika6192 <108252350+Ritika6192@users.noreply.github.com>
2022-12-23 17:51:17 +0530

    Meta tag added

--- index.php
+++ index.php
@@ -23,8 +23,9 @@
         <link href="assets/css/style.css" rel="stylesheet"/>
         <!-- bootstrap 5.1 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <meta name="google-site-verification" content="7dXHQhbQPnjX2g9MnlDRf8PHcP-ZOZy_uh1vJjfrV_8" />
     </head>
-    
+
     <body>
         <!-- navigation bar -->
         <nav class="navbar navbar-dark bg-transparent fixed-top">
